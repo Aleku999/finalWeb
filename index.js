fec3fdaf928bf5b32eed6d2d04f91e94f71d7347
Valetina <Aleku999@gmail.com>
2019-05-23 15:58:41 -0500

    Rutas dinamicas

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+
 var app = express();
+
 var renderMotor = require ('express-handlebars');
+
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }))
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'handlebars');
+
 app.engine('handlebars', renderMotor());
+
 var fs = require ('fs');
+
 var productos = [];
 productos.push({ 
   titulo: 'Perro',
@@ -26,4 +34,33 @@ productos.push({
     precio: '2000',
     imagen: "",
     descripcion: '',
+  });
+
+  
+app.get('/', function(request,response){
+    var contexto = {
+        titulo: 'Productos',
+        listaProductos: productos,
+        layout:false
+      };
+    response.render('index',contexto);
+    
+});
+app.get('/:producto', function(req,res){
+    var contexto={ layout:false};
+    productos.forEach(function(producto){
+  
+      if(producto.titulo == req.params.producto){
+        contexto.producto;
+      }
+    });
+ 
+      res.render('page',contexto);
+    
+    
+  });
+
+  
+app.listen(4000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
   });
